88e723f59f1809e977cf1c275287b41335ccb578
BautiCariaga23 <bautistaecariaga@gmail.com>
2023-11-20 22:53:13 -0300

    results

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -10,7 +10,7 @@ export default function Results(props: Props) {
     <div className='grid h-screen place-items-center gap-y-9 bg-white text-black p-6'>
         <div className='grid place-items-center'>
         <h1 className='text-8xl text-center'>Tu puntaje final fue:</h1>
-        <h2 className='text-7xl text-center'><span className='text-red-700'>{props.searchParams.points} </span> puntos de 536</h2>
+        <h2 className='text-7xl text-center'><span className='text-red-700'>{props.searchParams.points} </span> puntos</h2>
         <Link href={"/"}><div className='mt-16 text-center bg-gray-300 text-3xl sm:text-6xl rounded-lg p-2 hover:cursor-pointer hover:bg-gray-700 hover:text-white duration-300'>Volver a empezar</div>
         </Link>
         </div>
